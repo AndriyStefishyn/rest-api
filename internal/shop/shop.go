c879ef9636b3ae35ad795008067e5cd236676343
AndriyStefishyn <123456789andrij@gmail.com>
2025-03-13 16:26:03 +0100

    add shop.go

---
+++ internal/shop/shop.go
@@ -0,0 +1,9 @@
+package shop
+
+type Shop struct {
+	Id          string `bson:"_id" json:"_id,omitempty"`
+	Version     int    `bson:"version" json:"version"`
+	Name        string `bson:"name" json:"name"`
+	Location    string `bson:"location" json:"location"`
+	Description string `bson:"description" json:"description"`
+}
