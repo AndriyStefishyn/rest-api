7a64234e2d719c80f8f9f5f50daa91a00fd7a0cb
AndriyStefishyn <123456789andrij@gmail.com>
2025-03-17 13:09:55 +0100

    refactor methods name,delete comments

--- internal/shop/storage.go
+++ internal/shop/storage.go
@@ -5,10 +5,9 @@ import (
 )
 
 type Storage interface {
-	// TODO fix methods in interface
 	GetShopById(ctx context.Context, id string) (Shop, error)
-	GetShops(ctx context.Context) ([]Shop, error)
+	GetAllShops(ctx context.Context) ([]Shop, error)
 	InsertShop(ctx context.Context, shop Shop) error
 	UpdateShop(ctx context.Context, shop Shop) error
-	DeleteShop(ctx context.Context, shop Shop) error
+	DeleteShopById(ctx context.Context, id string) error
 }
