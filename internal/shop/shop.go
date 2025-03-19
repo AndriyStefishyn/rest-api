8078eb29f65a3232d7c57089b4be78d483398d2e
AndriyStefishyn <123456789andrij@gmail.com>
2025-03-19 17:10:46 +0100

    refactor test cases and updateShop function

--- internal/shop/shop.go
+++ internal/shop/shop.go
@@ -1,7 +1,7 @@
 package shop
 
 type Shop struct {
-	Id          string `bson:"_id" json:"_id"`
+	Id          string `bson:"_id" json:"id"`
 	Version     int    `bson:"version" json:"version"`
 	Name        string `bson:"name" json:"name"`
 	Location    string `bson:"location" json:"location"`
